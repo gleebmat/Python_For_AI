2fa4b4b75eccbf31313926d1fa7034f5f6b7727d
Gleb <glebsmailbox@gmail.com>
2026-05-06 20:17:35 +0200

    Started learning PyTorch

--- Python_for_AI/PyTorch/Stock_Price_Prediction/main.py
+++ Python_for_AI/PyTorch/Stock_Price_Prediction/main.py
@@ -7,6 +7,7 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 
+#
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 ticker = "AAPL"
 df = yf.download(ticker, "2020-01-01")
